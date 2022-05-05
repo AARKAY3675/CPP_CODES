0221962c3d7004e64d62ba059e6b3e3ed80afe4c
Rithik Sharma <20BCS9135@cuchd.in>
2022-05-05 11:43:58 +0530

    Update ClassesAndObjects.cpp

--- ClassesAndObjects.cpp
+++ ClassesAndObjects.cpp
@@ -1,19 +1,19 @@
 #include<iostream>
 using namespace std ;
-class Book {
-    public:
-    string title ;
+class Book {  // class declaration
+    public:   
+    string title ;  // class attributes 
     string author;
     int pages;
 };
 int main ()
 {
-    Book book1;
+    Book book1;  //object1 & its attributes
     book1.title = "Harry Potter";
     book1.author = "JK Rowling";
     book1.pages = 1200;
 
-    Book book2;
+    Book book2;  //object2 & its attributes
     book2.title = "12 Rules for Life";
     book2.author = "Jordan B Peterson";
     book2.pages = 400;
